d20c320e6583edb7eb0d66ac79b4e4140ab0be02
Marcelo Palacios <marcelouls@hotmail.com>
2021-12-29 01:21:41 -0300

    duplicidad produto

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Marketplace L6</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <style>
@@ -66,6 +67,14 @@
                         </li>
                         <li class="nav-item">
                             <span class="nav-link">{{auth()->user()->name}}</span>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('cart.index')}}" class="nav-link">
+                                @if (session()->has('cart'))
+                                    <span class="badge badge-danger">{{count(session()->get('cart'))}}</span>
+
+                                @endif
+                                <i class="fa fa-shopping-cart fa-2x"></i></a>
                         </li>
                     </ul>
                 </div>
